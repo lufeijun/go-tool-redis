e806bf4e1e548e0b7fbc8fb2c4b595f823ca6811
lufeijun1234 <lufeijun_1234@126.com>
2023-06-28 14:04:52 +0800

    ConnPool copy 完成了

--- redis/pool/conn.go
+++ redis/pool/conn.go
@@ -1,19 +1,37 @@
 package pool
 
 import (
+	"bufio"
 	"net"
+	"sync/atomic"
 	"time"
+
+	"github.com/lufeijun/go-tool-redis/redis/tool/proto"
 )
 
 type Conn struct {
 	usedAt  int64 // atomic
 	netConn net.Conn
 
-	// rd *proto.Reader
-	// bw *bufio.Writer
-	// wr *proto.Writer
+	rd *proto.Reader
+	bw *bufio.Writer
+	wr *proto.Writer
 
 	Inited    bool
 	pooled    bool
 	createdAt time.Time
+}
+
+// 链接使用时间
+func (cn *Conn) SetUsedAt(tm time.Time) {
+	atomic.StoreInt64(&cn.usedAt, tm.Unix())
+}
+func (cn *Conn) UsedAt() time.Time {
+	unix := atomic.LoadInt64(&cn.usedAt)
+	return time.Unix(unix, 0)
+}
+
+// 关闭连接
+func (cn *Conn) Close() error {
+	return cn.netConn.Close()
 }
